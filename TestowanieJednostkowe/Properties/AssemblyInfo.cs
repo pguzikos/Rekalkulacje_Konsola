bf3c49930c55bcf4985af4b642b00c07f28411e0
Piotr G <piotr.guzik@live.com>
2019-02-26 08:10:16 +0100

    POdstawowa struktura

---
+++ TestowanieJednostkowe/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestowanieJednostkowe")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestowanieJednostkowe")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9c8c1fa4-f872-4f7a-b32f-751a2bce0a36")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ klasaKalkulacji/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace klasaKalkulacji
+{
+    public class Class1
+    {
+    }
+}
